e209cda81eeb38a0e2ad0f8cfa51cf9ceaa9eb60
taguchi-k <k.taguchi@st-ventures.jp>
2017-02-17 13:58:35 +0900

    Add SampleCode

--- ios-uikit-uialertcontroller-uialertaction-demo/ViewController.swift
+++ ios-uikit-uialertcontroller-uialertaction-demo/ViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /// アラート
+    let alert = UIAlertController(title: "タイトル",
+                                  message: "本文",
+                                  preferredStyle: .alert)
+    /// アクションシート
+    let actionSheet = UIAlertController(title: "タイトル",
+                                        message: "本文",
+                                        preferredStyle: .actionSheet)
+    
+    /// キャンセルアクション（１つしかセットできない。キャンセル用）
+    let cancelAction = UIAlertAction(title: "cancel", style: .cancel) { _ in print("cancel") }
+    
+    /// デフォルトアクション（複数セット可能。通常のアクションはこちらを選択する）
+    let defaultAction = UIAlertAction(title: "default", style: .default) { _ in print("default") }
+    
+    /// ディストラクティブ（複数セット可能。アクションによってデータに破壊的な変更が発生する可能性あり）
+    let destructiveAction = UIAlertAction(title: "destructive", style: .destructive) { _ in print("destructive") }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: setup
+    
+    private func setup() {
+        // アラートにアクションをセットする
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        // defaultActionを強調表示にする
+        if #available(iOS 9.0, *) {
+            alert.preferredAction = defaultAction
+        } else {
+            print("iOS9未満です")
+        }
+        
+        // アクションシートにアクションをセットする
+        actionSheet.addAction(cancelAction)
+        actionSheet.addAction(defaultAction)
+        actionSheet.addAction(destructiveAction)
     }
-
-
+    
+    // MARK: ButtonAction
+    
+    @IBAction func didTapAlet(_ sender: UIButton) {
+        // アラートを表示する
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapActionSheet(_ sender: UIButton) {
+        // アクションシートを表示する
+        present(actionSheet, animated: true, completion: nil)
+    }
 }
 
